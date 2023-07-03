f212ce19fd0a3d02b3842b64e975f6b2705fb72b
Beenu <aryanpanikar9@gmail.com>
2023-07-03 15:57:33 +0530

    Set Vue-router

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,77 @@
 <template>
   <v-app id="inspire">
-    <v-navigation-drawer v-model="drawer" app>
-      <!--  -->
-    </v-navigation-drawer>
 
-    <v-app-bar app>
-      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+    <!-- <v-navigation-drawer app v-model="drawer">
+      <v-list-item>
+        <v-list-item-content>
+          <v-list-item-title class="text-h6">
+            User
+          </v-list-item-title>
+          <v-list-item-subtitle>
+            Score
+          </v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
 
-      <v-toolbar-title>Application</v-toolbar-title>
+      <v-divider></v-divider>
+
+      <v-list dense nav>
+        <v-list-item v-for="item in items" :to="item.to" :key="item.title" link>
+          <v-list-item-icon>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer> -->
+
+    <v-app-bar app dark src="board.jpg">
+      <template v-slot:img="{ props }">
+        <v-img v-bind="props" gradient="to top right, rgba(100,115,201,.7), rgba(25,32,72,.7)"></v-img>
+      </template>
+
+      <!-- <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon> -->
+
+      <v-app-bar-title class="title">ScoreUp</v-app-bar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-btn icon>
+        <v-icon>mdi-magnify</v-icon>
+      </v-btn>
+
+      <v-btn icon>
+        <v-icon>mdi-heart</v-icon>
+      </v-btn>
+
+      <v-btn icon>
+        <v-icon>mdi-dots-vertical</v-icon>
+      </v-btn>
+
+      <template v-slot:extension>
+        <v-tabs v-model="tab" align-with-title>
+          <v-tab v-for="item in items" :to="item.to" :key="item">{{ item.title }}</v-tab>
+        </v-tabs>
+      </template>
     </v-app-bar>
-
     <v-main>
-      <!--  -->
+      <router-view></router-view>
     </v-main>
   </v-app>
 </template>
 
 <script>
 export default {
-  data: () => ({ drawer: null }),
+  data: () => ({
+    drawer: null,
+    tab: null,
+    items: [
+      { title: 'Scoreboard', icon: 'mdi-counter', to: '/' },
+      { title: 'Submissions', icon: 'mdi-help-box', to: '/submissions' },
+    ],
+  }),
 }
 </script>
